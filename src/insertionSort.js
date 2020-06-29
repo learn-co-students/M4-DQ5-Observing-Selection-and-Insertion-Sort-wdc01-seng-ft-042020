72f9a4df5912c088ddb70c018e1f1f605ebe4ab3
agdsarge <agdsargeant@gmail.com>
2020-06-29 10:35:31 -0400

    Done.

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,25 @@
 function insertionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+    // for every item in the collection
+    // check if the previous item is greater than the current item
+    // if greater, the current item is not in place: swap it with the previous item
+    // repeat this process until the item is in the proper place
+    // only then move on to the next item in the collection
+    let greater
+    let lesser
+    for (let i = 1; i < arr.length; i+=1) {
+        if (arr[i] < arr[i - 1]) {
+
+            lesser = arr[i]
+            greater = arr[i - 1]
+            arr[i - 1] = lesser
+            arr[i] = greater
+            i -= 2
+
+        }
+
+    }
+    return arr
 }
+
+// let testArray = [4, 7, 5, 6, 3, 8, 9, 0, 2, 4, 2, 1]
+// console.log(insertionSort(testArray))
