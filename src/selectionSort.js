bc19d082144b2cdbae2d6e4c7564c017ab207505
epittab <ep7m@me.com>
2020-06-30 09:31:00 -0400

    solved

--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,17 @@
 function selectionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+
+  sortedArr = []
+  while (arr.length > 0) {
+    let smallest = Math.min(...arr)
+    sortedArr.push(smallest)
+    let index = arr.indexOf(smallest) 
+    arr.splice(index, 1)
+  }
+
+  return sortedArr
+
 }
+
+console.log(selectionSort([3, 5, 1]))
